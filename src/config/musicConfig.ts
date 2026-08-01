10f1b70123be52d530f0454d65f26e411e0970b6
Huomnh <805768041@qq.com>
2026-08-01 12:06:35 +0800

    更改音乐播放器模式为本地

--- src/config/musicConfig.ts
+++ src/config/musicConfig.ts
@@ -11,13 +11,13 @@ export const musicPlayerConfig: MusicPlayerConfig = {
 	showInNavbar: true,
 
 	// 使用方式："meting" 使用 Meting API，"local" 使用本地音乐列表
-	mode: "meting",
+	mode: "local",
 
 	// 默认音量 (0-1)
-	volume: 0.7,
+	volume: 0.5,
 
 	// 播放模式：'list'=列表循环, 'one'=单曲循环, 'random'=随机播放
-	playMode: "list",
+	playMode: "random",
 
 	// 是否显启用歌词
 	showLyrics: true,
